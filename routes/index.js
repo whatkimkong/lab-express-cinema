c3cd04c820c752a047c83d3583ed5bc2e86608e5
whatkimkong <whatkimkong@gmail.com>
2021-08-25 12:46:28 +0200

    Iteration 3 and 4 attempted but routing not working - confused about where and how to do them

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const MovieModel = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+/* GET /movies route */
+router.get('/', (req, res, next) => {
+    MovieModel.find()
+    .then((movies) => {
+        console.log(movies);
+        res.render('movies', {movies})
+    })
+    .catch ((err) => console.log(err))
+});
+
+router.get('/movies', (req, res, next) => {
+    MovieModel.find()
+    .then((movies) => {
+        res.redirect('/movies/details', {movies})
+    })
+    .catch((err) => console.log(err))
+})
+
+
 
 module.exports = router;
